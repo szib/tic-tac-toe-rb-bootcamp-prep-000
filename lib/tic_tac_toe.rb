c36413f228ad24df2fb0395e124bee7b5d65fd78
Ivan Szebenszki <szib77@gmail.com>
2019-03-04 22:29:35 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -86,7 +86,7 @@ def turn(board)
 end
 
 def play(board)
-  until over?(board)
+  while !over?(board)
     puts "Congratulations #{winner(board)}!"if won?(board)
     puts "Cat's Game!" if draw?(board)
     turn(board)
